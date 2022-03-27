38d26f5fe6b30ade482a9de389f5b1f2b06e2219
Lucas Gabriel Teixeira da Silva <s.lucasgteixeira@gmail.com>
2022-03-27 01:06:24 -0300

    Functions and BiFunctions

--- src/main/java/functionalInterface/_Function.java
+++ src/main/java/functionalInterface/_Function.java
@@ -1,9 +1,12 @@
 package functionalInterface;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class _Function {
     public static void main(String[] args) {
+
+        //Function that takes one argument and return one result
         int increment = incrementByOne(1);
         System.out.println(increment);
 
@@ -13,19 +16,39 @@ public class _Function {
         int multiply = multiplyBy10Function.apply(increment2);
         System.out.println(multiply);
 
-        Function<Integer, Integer> addByOneAndThenMultiplyBy10 =
-                incrementByOneFunction.andThen(multiplyBy10Function);
-
         System.out.println(addByOneAndThenMultiplyBy10.apply(4));
+
+        //BiFunction that takes two arguments and return one result
+
+        System.out.println(incrementByOneAndMultiply(4, 100));
+
+        Integer incrementAndMultiply = incrementByOneAndMultiplyBiFunction.apply(4, 100);
+        System.out.println(incrementAndMultiply);
+
+
     }
 
+    //Functions
     static Function<Integer, Integer> incrementByOneFunction =
             number -> number + 1;
 
     static Function<Integer, Integer> multiplyBy10Function =
             number -> number * 10;
 
+    //BiFunctions
+    static Function<Integer, Integer> addByOneAndThenMultiplyBy10 =
+            incrementByOneFunction.andThen(multiplyBy10Function);
+
+    static BiFunction<Integer, Integer, Integer> incrementByOneAndMultiplyBiFunction =
+            (numberToIncrementByOne, numberToMultiplyBy)
+                    -> (numberToIncrementByOne + 1) * numberToMultiplyBy;
+
+    //Normal java Functions
     static int incrementByOne(int number){
         return number + 1;
+    }
+
+    static int incrementByOneAndMultiply(int number, int numToMultiplyBy){
+        return (number + 1) * numToMultiplyBy;
     }
 }
